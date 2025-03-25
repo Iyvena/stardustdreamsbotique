f240bd3c1d46365c9def1fefc535c4448f7c1e74
kpisti18 <k.istvanzoltan@gmail.com>
2025-03-25 13:51:51 +0100

    bigfixes in productsController.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -8,10 +8,10 @@ const { deleteProduct } = require("../controllers/productsController");
 
 const router = express.Router();
 //termék feltöltése és ellenörzése hogy admin e 
-router.get('/getALLproduct', getALLproduct);
+router.get('/getALLproduct', authenticateToken, getALLproduct);
 router.post('/uploadProduct', authenticateToken, isAdmin, upload.single('productImage'), uploadProduct);
 router.get("/filter", filterProducts);
-router.delete('/:product_id', deleteProduct);
+router.delete('/:product_id', authenticateToken, deleteProduct);
 
 
 module.exports = router;
