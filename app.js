093ea4ff562b8d46d4cd6e21dd996a2aee222c4c
diak <diak@mesterlevel.lo>
2025-04-03 08:41:39 +0200

    cart

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const authRoutes = require('./routes/authRoutes');
 const likeRoutes = require('./routes/likedRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-const orderItemsRoutes = require('./routes/orderItemsRoutes');
+/*const orderItemsRoutes = require('./routes/orderItemsRoutes');*/
 const cartRoutes = require('./routes/cartRoutes');
-const cartItemsRoutes = require('./routes/cartItemsRoutes');
+/*const cartItemsRoutes = require('./routes/cartItemsRoutes');*/
 //const forgotPasswordRoutes = require("./routes/forgotPasswordRoutes");
 //const resetPasswordRoutes = require("./routes/resetPasswordRoutes");
 const productDescriptionRoutes = require('./routes/productDescriptionRoutes');
@@ -47,9 +47,9 @@ app.use("/api/profile", editProfileRoutes);
 app.use('/api/likes', likeRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/order_items', orderItemsRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/cart-items', cartItemsRoutes);
+/*app.use('/api/order_items', orderItemsRoutes);*/
+app.use('/cart', cartRoutes);
+/*app.use('/api/cart-items', cartItemsRoutes);*/
 //app.use("/api/auth", forgotPasswordRoutes);  // Forgot password route
 //app.use("/api/auth", resetPasswordRoutes);   // Reset password route
 app.use('/', productDescriptionRoutes);
